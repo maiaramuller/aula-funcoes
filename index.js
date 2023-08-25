bacbf133de2cbcb790f599e166fb6d13b24baffe
maiara <maiara.15muller@outlook.com>
2023-08-24 21:56:38 -0300

    aula 12

---
+++ index.js
@@ -0,0 +1,19 @@
+//Exercícios de interpretação de código
+/*1.
+a)10 e 50
+b)irá aparecer nada
+*/
+
+/*2.
+a)A função está fazendo com que retorne a string em letras minúsculas e determona que se caso encotre a palavra cenoura retorne true no console.
+b)true,true,true
+*/
+
+//Exercícios de escrita de código
+
+//1.
+function sobreMim() {
+  const maiara =
+    "Eu sou Maiara, tenho 22 anos, moro em São Leopoldo e sou estudante.";
+}
+console.log(maiara);
